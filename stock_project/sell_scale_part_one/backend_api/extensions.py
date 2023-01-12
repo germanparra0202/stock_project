327300e425dbce1e3aef376dd817e804c7a13982
German Parra <78463807+germanparra0202@users.noreply.github.com>
2023-01-12 16:50:57 -0700

    Delete stock_project/sell_scale_part_one/backend_api directory

--- stock_project/sell_scale_part_one/backend_api/extensions.py
+++
@@ -1,7 +0,0 @@
-''' The intention of this file is that the 
-    project doesn't run into any circular 
-    errors.'''
-from flask_sqlalchemy import SQLAlchemy 
-from sqlalchemy import and_
-
-db = SQLAlchemy()
